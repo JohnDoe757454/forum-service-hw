c64cb617374ba58b22d143da9cabf32962faf483
stanislavsigal <stanislavsigal@172.24.224.45>
2024-05-16 19:27:59 +0300

    Added accounting dtos

---
+++ src/main/java/telran/java52/accounting/dto/UserRegisterDto.java
@@ -0,0 +1,14 @@
+package telran.java52.accounting.dto;
+
+import lombok.Getter;
+
+@Getter
+
+public class UserRegisterDto {
+	{
+		String login;
+		String password;
+		String firstName;
+		String lastName;
+	}
+}
