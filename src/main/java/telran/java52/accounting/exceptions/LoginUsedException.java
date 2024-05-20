e6d14a8481cda36e713d7204d7e39aa88cfcde46
stanislavsigal <stanislavsigal@stanislavs-mbp>
2024-05-20 10:52:50 +0300

    HW fixes

--- src/main/java/telran/java52/accounting/exceptions/LoginUsedException.java
+++ src/main/java/telran/java52/accounting/exceptions/LoginUsedException.java
@@ -3,7 +3,7 @@ package telran.java52.accounting.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.FORBIDDEN)
+@ResponseStatus(HttpStatus.CONFLICT)
 public class LoginUsedException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
