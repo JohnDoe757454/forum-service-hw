a85ec21e4f7109b1061fccbdc0fb30c6ec74290c
stanislavsigal <stanislavsigal@stanislavs-mbp>
2024-05-20 18:58:00 +0300

    added bcrypt

--- src/main/java/telran/java52/accounting/service/UserAcountSeviceImp.java
+++ src/main/java/telran/java52/accounting/service/UserAcountSeviceImp.java
@@ -2,6 +2,7 @@ package telran.java52.accounting.service;
 
 import java.util.Optional;
 
+import org.mindrot.jbcrypt.BCrypt;
 import org.modelmapper.ModelMapper;
 
 import org.springframework.stereotype.Service;
@@ -12,6 +13,7 @@ import telran.java52.accounting.dto.RolesDto;
 import telran.java52.accounting.dto.UserDto;
 import telran.java52.accounting.dto.UserEditDto;
 import telran.java52.accounting.dto.UserRegisterDto;
+import telran.java52.accounting.exceptions.IncorrectRoleException;
 import telran.java52.accounting.exceptions.LoginUsedException;
 import telran.java52.accounting.exceptions.UserNotFoundException;
 import telran.java52.accounting.model.UserAccount;
@@ -27,6 +29,8 @@ public class UserAcountSeviceImp implements UserAccountService {
 	public UserDto register(UserRegisterDto userRegisterDto) {
 		if (userRepository.findById(userRegisterDto.getLogin()).isEmpty()) {
 			UserAccount user = modelMapper.map(userRegisterDto, UserAccount.class);
+			String password = BCrypt.hashpw(userRegisterDto.getPassword(), BCrypt.gensalt());
+			user.setPassword(password);
 			userRepository.save(user);
 			return modelMapper.map(user, UserDto.class);
 		}
@@ -63,10 +67,14 @@ public class UserAcountSeviceImp implements UserAccountService {
 	@Override
 	public RolesDto changeRolesList(String login, String role, boolean isAddRole) {
 		UserAccount user = userRepository.findById(login).orElseThrow(UserNotFoundException::new);
-		if (isAddRole) {
-			user.addRole(role);
-		} else {
-			user.removeRole(role);
+		try {
+			if (isAddRole) {
+				user.addRole(role);
+			} else {
+				user.removeRole(role);
+			}
+		} catch (Exception e) {
+			throw new IncorrectRoleException();
 		}
 		userRepository.save(user);
 		return modelMapper.map(user, RolesDto.class);
@@ -76,7 +84,8 @@ public class UserAcountSeviceImp implements UserAccountService {
 	public void changePassword(String login, String newPassword) {
 		UserAccount user = userRepository.findById(login).orElseThrow(UserNotFoundException::new);
 		if (newPassword != null) {
-			user.setPassword(newPassword);
+			String password = BCrypt.hashpw(newPassword, BCrypt.gensalt());
+			user.setPassword(password);
 			userRepository.save(user);
 		}
 
